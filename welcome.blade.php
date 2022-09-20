f503fbf5ca5294c602245ccc35f72304a9d5a41b
HelloEliin <98847181+HelloEliin@users.noreply.github.com>
2022-09-20 13:34:34 +0200

    Update welcome.blade.php

--- welcome.blade.php
+++ welcome.blade.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html>
 <body>
+  <head><script src="https://kit.fontawesome.com/0fe9543042.js" crossorigin="anonymous"></script></head>
 <link rel="stylesheet" href="style.css">
 <br>
 <br>
 <h1>WATERMARK</h1>
 <form method="post" action="{{route('upload.uploadfile')}}" enctype="multipart/form-data"><br><br>
   @csrf
-  Välj fil att ladda upp:
+  <i class="fa-solid fa-camera"></i> Välj bild: 
   <br>
   <br>
   <input type="file" name="file">
+  
 
 <br>
 <br>
+<br>
   
-        <label for="stamp">Välj färg på loggan: </label> <br><br>
+<i class="fa-solid fa-stamp"></i> <label for="stamp">Välj färg på loggan: </label> <br><br>
 
          <select name="stamp" id="stamp">
          <option value="svart">Svart</option>
@@ -24,17 +27,22 @@
          <br>
          <br>
          <br>
+         <br>
 
-         <label for="size">Välj storlek: </label> <br><br>
+         <i class="fa-regular fa-crop-simple"> </i><label for="size"> Välj storlek: </label> <br><br>
 
          <select name="size" id="size">
          <option value="facebook">Facebook</option>
          <option value="instagram">Instagram</option>
 </select>
 <br>
-<br>     <input type="submit" value="Skapa!" name="submit"><br><br>
+<br>     <i class="fa-solid fa-arrow-down"></i>
+<br>
+<br>
+<input type="submit" value="Skapa!" name="submit"><br><br>
 
 
 </form>
 </body>
 </html>
+
