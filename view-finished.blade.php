4d0db579444ff9e1ee8a2b48fe99be486af5f720
HelloEliin <98847181+HelloEliin@users.noreply.github.com>
2022-09-20 12:36:12 +0200

    Add files via upload

---
+++ view-finished.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/0fe9543042.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <br>
+    <br>
+    <br>
+
+<img src="{{$img}}"> <br><br><br>
+<br>
+<br>
+<br>
+
+
+<i class="fa-solid fa-download"><br><br>
+<a href="{{$img}}" download> DOWNLOAD</a>
+
+
+
+
+
+
+
+
+</body>
+</html>
