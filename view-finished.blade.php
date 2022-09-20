f4091d8dc93c31fff50901369a34eab3a546f725
HelloEliin <98847181+HelloEliin@users.noreply.github.com>
2022-09-20 14:51:20 +0200

    Update view-finished.blade.php

--- view-finished.blade.php
+++ view-finished.blade.php
@@ -1,30 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="style.css">
-<script src="https://kit.fontawesome.com/0fe9543042.js" crossorigin="anonymous"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/0fe9543042.js" crossorigin="anonymous"></script>
+
 <body>
-    <br>
-    <br>
-    <br>
 
-<img src="{{$img}}"> <br><br><br>
-<br>
-<br>
-<br>
-
+    <br>
+    <br>
+    <br>
+    
+    <img src="{{$img}}"> <br><br><br>
+    <br>
+    <br>
+    <br>
 
 <i class="fa-solid fa-download"><br><br>
 <a href="{{$img}}" download> DOWNLOAD</a>
-
-
-
-
-
-
 
 
 </body>
